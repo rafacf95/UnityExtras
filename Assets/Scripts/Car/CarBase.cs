573ee1a9d23e02a3f85a858057d803fd93a3824e
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-24 14:46:02 -0300

    Create CarBase.cs

---
+++ Assets/Scripts/Car/CarBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBase : MonoBehaviour
+{
+
+    public int speed = 20;
+    public int gear = 5;
+    public GameObject carbasePrefab;
+
+    public int TotalSpeed
+    {
+        get { return speed * gear; }
+    }
+
+    public void CreateCar()
+    {
+        var a = Instantiate(carbasePrefab);
+        a.transform.position = Vector3.zero;
+    }
+}
