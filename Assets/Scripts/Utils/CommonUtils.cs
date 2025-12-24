17021b7406c4d715553f505251a0d4470477d786
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-24 15:34:02 -0300

    Update CommonUtils.cs

--- Assets/Scripts/Utils/CommonUtils.cs
+++ Assets/Scripts/Utils/CommonUtils.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using UnityEditor;
 
 public static class CommonUtils
 {
@@ -10,4 +11,27 @@ public static class CommonUtils
     {
         return list[Random.Range(0, list.Count)];
     }
+
+#if UNITY_EDITOR
+    [MenuItem("rafacf95/Create Cube %g")]
+    public static void CreateCube()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = Vector3.zero;
+    }
+
+    [MenuItem("rafacf95/Create Car Prefab %#g")]
+    public static void CreateCarPrefab()
+    {
+        GameObject prefab = Resources.Load<GameObject>("Car");
+        if (prefab != null)
+        {
+            GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+        }
+        else
+        {
+            Debug.LogError("Prefab not found!");
+        }
+    }
+#endif
 }
